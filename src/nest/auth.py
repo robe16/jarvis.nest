47959b32a66ed9fe64b2600f355b65d8e3e878ab
Rob <robe16@hotmail.co.uk>
2018-01-31 21:59:51 +0000

    Update to allow semi-manual OAuth process to be completed.

--- src/nest/auth.py
+++ src/nest/auth.py
@@ -2,10 +2,11 @@ import requests
 import json
 import datetime
 from log.log import log_outbound
-from config.config import get_cfg_details_clientId, get_cfg_details_clientSecret
+from config.config import get_cfg_details_clientId, get_cfg_details_clientSecret, set_cfg_details_oauth
+from resources.global_resources.log_vars import logPass, logFail, logException
 
 
-def get_accesstoken_pincode(pincode):
+def get_accesstoken_pincode(state, pincode):
     url_access = 'https://api.home.nest.com/oauth2/access_token'
     #
     headers = {'Connection': 'close',
@@ -22,7 +23,12 @@ def get_accesstoken_pincode(pincode):
                       data=payload)
     #
     r_pass = (r.status_code == requests.codes.ok)
-    log_outbound(r_pass, url_access, '-', 'POST', r.status_code)
+    #
+    result = logPass if r_pass else logFail
+    log_outbound(result,
+                 url_access, '', 'POST', '',
+                 '-', '-',
+                 r.status_code)
     #
     if not r_pass:
         raise Exception()
@@ -30,11 +36,21 @@ def get_accesstoken_pincode(pincode):
     try:
         response = json.loads(r.text)
     except Exception as e:
-        log_outbound(r_pass, url_access, '-', 'POST', r.status_code, exception=e)
+        log_outbound(logException,
+                     url_access, '', 'POST', '',
+                     '-', '-',
+                     r.status_code,
+                     exception=e)
         raise Exception()
     #
     token = response['access_token']
     tokenexpiry = datetime.datetime.now() + datetime.timedelta(seconds=response['expires_in'])
     #
+    set_cfg_details_oauth(token, tokenexpiry, state)
+    #
     return {'token': token,
             'tokenexpiry': tokenexpiry}
+
+state = input('Enter State: ')
+pincode = input('Enter Pin Code: ')
+get_accesstoken_pincode(state, pincode)
